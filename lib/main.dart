d8f751148edc41d3d94dcb1b5ec4c23af7bb2167
Nezas <nedas.zaksauskas@gmail.com>
2021-01-27 10:25:45 +0200

    Added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -28,23 +28,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
-      title: Text(
-        'Overview',
-        style: TextStyle(fontSize: 32),
-      ),
-    );
+        title: Text(
+          'Overview',
+          style: TextStyle(fontSize: 32),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              print("Settings");
+            },
+          ),
+        ]);
     return Scaffold(
       appBar: appBar,
       body: Container(
         alignment: Alignment.center,
-          margin: EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              Text("4234 Eur", style: TextStyle(fontSize: 42),),
-              Text("Balance", style: TextStyle(fontSize: 20),),
-            ],
-          ),
+        margin: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "0 €",
+              style: TextStyle(fontSize: 42),
+            ),
+            Text(
+              "Balance",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                  label: Text("Add money"),
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    print("Add");
+                  },
+                ),
+                ElevatedButton.icon(
+                  label: Text("Add expenses"),
+                  icon: Icon(Icons.remove),
+                  onPressed: () {
+                    print("Remove");
+                  },
+                ),
+              ],
+            )
+          ],
         ),
+      ),
     );
   }
 }
